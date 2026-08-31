ca0b7e11dfeab183dbdc99c124c6dd781ec72f3f
Pi Agent <pi@agent.local>
2026-08-31 16:18:02 +0800

    pi: update tests\section-22-rust-ast.ts

--- tests/section-22-rust-ast.ts
+++ tests/section-22-rust-ast.ts
@@ -2,7 +2,7 @@
 
 import * as fs from "node:fs";
 import * as path from "node:path";
-import { extractDocumentSymbols, findSymbolReferences, extractLocalSymbolHover } from "../retrieval/ast_search";
+import { extractDocumentSymbols, findSymbolReferences, extractLocalSymbolHover } from "../src/retrieval/ast_search";
 import { createTestWorkspace, runSection, assertPass, logPass } from "./_setup";
 
 async function main(): Promise<void> {
