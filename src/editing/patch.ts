5d4a3c03b00e8c661191791e803061709451e4dd
Pi Agent <pi@agent.local>
2026-08-31 15:30:05 +0800

    pi: update src\editing\patch.ts

--- src/editing/patch.ts
+++ src/editing/patch.ts
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 import * as path from "path";
 import * as diff from "diff";
-import { checkSyntaxContent } from "./syntax-verify"
+import { checkSyntaxContent } from "./syntax-verify";
 
 /**
  * Result of composing and validating a surgical patch.
